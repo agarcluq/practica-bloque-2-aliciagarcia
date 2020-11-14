38a4db16de0792226c2f815bb3b2556e58a3e0f0
agarcluq <agarcluq@everis.com>
2020-11-14 18:55:07 +0100

    1 file modified

--- script.js
+++ script.js
@@ -11,6 +11,21 @@ class User{
    }
 }
 // Una vez esté lista la clase tenéis que crear una instancia de dicha clase para cada uno de los usuarios que obtengáis del documento "users.json" usando el método fetch (al igual que hicistéis en el ejercicio 3).
+/*
+function getUsers() {
+    fetch("./users.json")
+    .then(getResponseJsonData)
+    .then(showMessage);
+}
+function getResponseJsonData(response) {
+  return response.json();
+}
+
+function showMessage(data) {
+  console.log(`El nombre del usuario es ${users.name}`);
+}*/
+
+
 
 function getUsers() {
   fetch("./users.json")
@@ -45,6 +60,7 @@ function esSuficientementeGrande(data) {
   //return data[1]>= 500;
 }
 
+getUsers();
 // Al recorrer el array de usuarios, en cada iteración, debéis usar las propiedades "name" y "money" del usuario del json para añadirle sus valores al constructor en el parámetro que establezca el valor de la propiedad del objeto con el mismo nombre.
 // Por ejemplo: Para instanciar el primer usuario de la lista haremos "new User(usuarioDelJson.name, usuarioDelJson.money, metodoDelusuario)".
 // El tercer parámetro de cada objeto recibirá una función que sacará con un alert la frase "dinero del usuario" (opcionalmente podéis sustituir esta frase por el dinero que tiene el usuario de esa instancia). Esta función la declararéis fuera de la clase con el nombre "showMeTheMoneyFunc".
